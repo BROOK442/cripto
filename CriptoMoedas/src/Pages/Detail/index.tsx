4549cc867e6d62f198faaff006d7d7bec851f020
João Teles <104568838+BROOK442@users.noreply.github.com>
2023-10-24 13:34:17 +0000

    ...

---
+++ CriptoMoedas/src/Pages/Detail/index.tsx
@@ -0,0 +1,19 @@
+import style from './detail.module.css'
+import {useParams} from 'react-router-dom'
+import { useEffect } from 'react';
+
+export function Detail (){
+    const {cripto} = useParams();
+
+    useEffect(() =>{
+        function getData() {
+            fetch(` `)
+        }
+    })
+
+    return(
+        <div>
+            <h1>Pagina de detalhes {cripto}</h1>
+        </div>
+    )
+}
